b5f518e691f1fdddae7ad08665cd05b683538d11
cladnic <cladnic@hotmail.com>
2022-10-27 21:15:53 +0200

    Convert player to kineticbody2d and shuriken to area2d

--- src/FlyingGame/Actors/Player/Player.gd
+++ src/FlyingGame/Actors/Player/Player.gd
@@ -1,30 +1,34 @@
-extends AnimatedSprite
-var shuriken = preload("res://src/FlyingGame/Objects/MovingObjects/Shuriken.tscn")
+extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var projectile: PackedScene
+
 var speed = 200 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position += Vector2(0.05,0.05)
-	
+	position += Vector2(0.0, 0.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	position += Vector2(10,0) * delta
 	if Input.is_action_pressed("ui_left"):
-		position += Vector2(-1,0) * delta * speed 
+		position += Vector2(-1,0) * delta * speed
+		transform.x = Vector2(-1, 0)
 	if Input.is_action_pressed("ui_right"):
 		position += Vector2(1,0) * delta * speed 
+		transform.x = Vector2(1.0,0)
 	if Input.is_action_pressed("ui_down"):
-		position += Vector2(0,1) * delta * speed 
+		position += Vector2(0,1) * delta * speed
 	if Input.is_action_pressed("ui_up"):
-		position += Vector2(0,-1) * delta * speed 
+		position += Vector2(0,-1) * delta * speed
 	if Input.is_action_just_pressed("ui_select"):
-			var shuriken_i = shuriken.instance()
-			shuriken_i.shoot(global_position,500) 
-			var level_root = get_parent().get_parent()
-			level_root.add_child(shuriken_i)
+		shoot()
 
+func shoot():
+	# Instantiate a projectile and set its direction
+	var proj = projectile.instance()
+	proj.set_shoot_direction(transform.x)
+	
+	# Spawn the projectile object, and set the position
+	self.get_parent().add_child(proj)
+	proj.transform = $Player.global_transform
--- src/FlyingGame/Objects/MovingObjects/Shuriken.gd
+++ src/FlyingGame/Objects/MovingObjects/Shuriken.gd
@@ -1,21 +1,16 @@
-extends Sprite
+extends Area2D
 
+var direction: Vector2
 
 var velocity = 0
 var rot = PI
-var speed = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var speed = 750
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += Vector2(1,0) * delta * speed  
-	rotation += rot * delta
-	
-func shoot(pos,speedv):
-	speed = speedv 
-	position = pos 
+func _physics_process(delta):
+	position += direction * speed * delta
+	$Sprite.rotation += rot * delta
+
+func set_shoot_direction(new_direction):
+	self.direction = new_direction
 	
