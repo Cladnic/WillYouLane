2dfcd05f7c648b51369ccdef72317e98c3bdcaf1
cladnic <cladnic@hotmail.com>
2022-10-28 22:14:20 +0200

    changed a bit of movement for the player and added tilemap

--- src/FlyingGame/Actors/Player/Player.gd
+++ src/FlyingGame/Actors/Player/Player.gd
@@ -2,33 +2,34 @@ extends KinematicBody2D
 
 export var projectile: PackedScene
 
-var speed = 200 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	position += Vector2(0.0, 0.0)
+var speed = 20000
+var velocity := Vector2(0.0, 0.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	position += Vector2(10,0) * delta
-	if Input.is_action_pressed("ui_left"):
-		position += Vector2(-1,0) * delta * speed
-		transform.x = Vector2(-1, 0)
-	if Input.is_action_pressed("ui_right"):
-		position += Vector2(1,0) * delta * speed 
-		transform.x = Vector2(1.0,0)
-	if Input.is_action_pressed("ui_down"):
-		position += Vector2(0,1) * delta * speed
-	if Input.is_action_pressed("ui_up"):
-		position += Vector2(0,-1) * delta * speed
+	var direction = get_move_direction()
+	set_look_direction(direction)
+	velocity = delta * speed * direction
+	move_and_slide(velocity)
 	if Input.is_action_just_pressed("ui_select"):
 		shoot()
+
+func get_move_direction():
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	
+func set_look_direction(direction: Vector2):
+	if direction.x < 0:
+		transform.x = Vector2(-1,0)
+	elif direction.x > 0:
+		transform.x = Vector2(1,0)
 
 func shoot():
 	# Instantiate a projectile and set its direction
 	var proj = projectile.instance()
 	proj.set_shoot_direction(transform.x)
-	
 	# Spawn the projectile object, and set the position
 	self.get_parent().add_child(proj)
 	proj.transform = $Player.global_transform
